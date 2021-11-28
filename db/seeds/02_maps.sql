bfa20a07303cdff979c6238e203345f0fb9dc1cc
Youssef Nafaa <nafaayoussef@gmail.com>
2021-11-27 22:13:59 -0500

    create and seed tables

---
+++ db/seeds/02_maps.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO maps (title, description, longitude, latitude, created_on, user_id)
+  VALUES ('Map Ottawa', 'Nice Map','45.424721', '-75.695000', '2021-01-11 20:00:00', 2);
+INSERT INTO maps (title, description, longitude, latitude, created_on, user_id)
+  VALUES ('Map Toronto', 'Beautiful Map','43.651070','-79.347015', '2021-02-11 14:30:00', 1);
+INSERT INTO maps (title, description, longitude, latitude, created_on, user_id)
+  VALUES ('Map Moscow', 'Fantastic Map','55.751244', '37.618423', '2021-03-11 09:30:00', 1);
+INSERT INTO maps (title, description, longitude, latitude, created_on, user_id)
+  VALUES ('Map Casablanca', 'Cool Map','33.589886', '-7.603869', '2021-02-11 14:30:00', 4);
