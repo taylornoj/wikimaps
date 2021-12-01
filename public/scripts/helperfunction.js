b07f40186207a97ba3c5a0c70bd92898f7c324b3
Taylor Noj <taylornoj@gmail.com>
2021-12-01 17:33:22 +0000

    final functionality for fave button

---
+++ public/scripts/helperfunction.js
@@ -0,0 +1,11 @@
+$(function() {
+  $(".heart-container").find(".fa-heart").click(function() {
+    if($(this).hasClass("far")) {
+      $(this).removeClass("far")
+      $(this).addClass("fas")
+    } else {
+      $(this).removeClass("fas")
+      $(this).addClass("far")
+    }
+  });
+});
