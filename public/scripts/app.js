25f9ffa9ffa02a7da6b04daf62cf1a6189f6e62e
rom <mozzieisme@gmail.com>
2021-12-01 21:22:37 -0500

    Add delete and create function to the map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,2 +1,49 @@
 // Client facing scripts here
 
+
+
+
+const initMap = function(lat, lng, mapId) {
+  const zoom = 10;
+  const lat_long_Ottawa = { lat: lat, lng: lng };
+  // const lat_long_Toronto = { lat: 43.65107, lng: -79.347015 };
+  // const lat_long_Casablanca = { lat: 33.589886, lng: -7.603869 };
+
+  const coordsOttawa = {
+    zoom: zoom,
+    center: lat_long_Ottawa,
+  };
+  const map = new google.maps.Map(
+    document.getElementById(mapId),
+    coordsOttawa
+  );
+
+  // const coordsToronto = {
+  //   zoom: zoom,
+  //   center: lat_long_Toronto,
+  // };
+  // const map2 = new google.maps.Map(
+  //   document.getElementById("map2"),
+  //   coordsToronto
+  // );
+
+  // const coordsCasablanca = {
+  //   zoom: zoom,
+  //   center: lat_long_Casablanca,
+  // };
+  // const map3 = new google.maps.Map(
+  //   document.getElementById("map3"),
+  //   coordsCasablanca
+  // );
+
+  const addMarker = (coords, map) => {
+    const marker = new google.maps.Marker({
+      position: coords,
+      map: map,
+    });
+  };
+
+  addMarker(lat_long_Ottawa, map);
+  // addMarker(lat_long_Toronto, map2);
+  // addMarker(lat_long_Casablanca, map3);
+};
--- server.js
+++ server.js
@@ -3,14 +3,9 @@ require("dotenv").config();
 
 // Web server config
 
-
-
 // Install the cookie-session ::
 // npm install cookie-session ::
-const cookieSession = require('cookie-session');
-
-
-
+const cookieSession = require("cookie-session");
 
 const PORT = process.env.PORT || 8080;
 const sassMiddleware = require("./lib/sass-middleware");
@@ -45,12 +40,13 @@ app.use(
 
 app.use(express.static("public"));
 
-
 // +++++++++++ Add this cookie here ::
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1', 'key2']
-}));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1", "key2"],
+  })
+);
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -59,7 +55,6 @@ const mapsRoutes = require("./routes/maps");
 const markersRoutes = require("./routes/markers");
 const favouritesRoutes = require("./routes/favourites");
 
-
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
@@ -67,16 +62,11 @@ app.use("/api/maps", mapsRoutes(db));
 app.use("/api/markers", markersRoutes(db));
 app.use("/api/favourites", favouritesRoutes(db));
 
-
 // Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
-
-
-
-
 
 // Delete after DB works ::
 const users = {
@@ -90,25 +80,15 @@ const users = {
   },
 };
 
-
-
-
-
 app.get("/", (req, res) => {
   const user = req.session.id;
-  res.render("index", {user: user});
+  res.render("index", { user: user });
 });
-
-
-
-
-
 
 app.get("/login", (req, res) => {
   const user = req.session.id;
-  res.render("login", {user: user});
+  res.render("login", { user: user });
 });
-
 
 app.post("/login", (req, res) => {
   const body = req.body;
@@ -116,15 +96,10 @@ app.post("/login", (req, res) => {
   res.redirect("/");
 });
 
-
-
-
-
 app.get("/register", (req, res) => {
   const user = req.session.id;
-  res.render("register", {user: user});
+  res.render("register", { user: user });
 });
-
 
 app.post("/register", (req, res) => {
   const newUser = {
@@ -135,39 +110,102 @@ app.post("/register", (req, res) => {
   res.redirect("/login");
 });
 
-
-
-
-
 // LogOut ::
 app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect("/");
 });
 
+////////////       TESTING!!!! ::::               ////////////////////////////////////
 
 
 
 
-app.get("/createmap", (req, res) => {
-  const user = req.session.id;
-  res.render("createmap", {user: user});
-});
+
+const generateRandomString = function() {
+  const arr = [
+    "d87s8d",
+    "df923j",
+    "345gfg",
+    "345gdf",
+    "567ytr",
+    "34fdsf",
+    "34dfsf",
+    "34fknd",
+    "34sskf",
+    "34f32f",
+    "3dfdsf",
+    "34fsds",
+    "34fdsf",
+    "3d234f",
+    "34f5sf",
+    "df3fds",
+    "sd3dsf",
+    "756hsf",
+    "sdf345",
+    "344fds",
+  ];
+
+  let randomNum = Math.random() * 20;
+  let roundNumber = Math.floor(randomNum);
+
+  return arr[roundNumber];
+};
 
 
 
 
+
+
+
+const cardDatabase = {
+  b6UTxQ: {
+    userID: "aJ48lW",
+    title: "Favourite Parks in Toronto",
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    longitude: 43.65107,
+    latitude: -79.347015,
+    id: "b6UTxQ"
+  },
+  b6UTxQ2: {
+    userID: "aJ48lW",
+    title: "Favourite Parks in Toronto",
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    longitude: 43.65107,
+    latitude: -79.347015,
+    id: "b6UTxQ2"
+  },
+  b6UTxQ3: {
+    userID: "aJ48lW",
+    title: "Favourite Parks in Toronto",
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    longitude: 43.65107,
+    latitude: -79.347015,
+    id: "b6UTxQ3"
+  },
+};
+
+// console.log(cardDatabase.b6UTxQ.longitude);
+
+// const coordinates = cardDatabase.b6UTxQ.longitude
+// module.exports = {coordinates}
 
 app.get("/maps", (req, res) => {
   const user = req.session.id;
-  res.render("maps_index", {user: user});
+  res.render("maps_index", { user: user, cards: cardDatabase });
 });
 
+/////////////////////////////////////////////
 
+app.get("/createmap", (req, res) => {
+  const user = req.session.id;
+  res.render("createmap", { user: user });
+});
 
-
-
-app.post("/createmap" , (req, res) => {
+app.post("/createmap", (req, res) => {
   console.log("title", req.body.title);
   const title = req.body.title;
   const description = req.body.description;
@@ -175,34 +213,42 @@ app.post("/createmap" , (req, res) => {
   const latitude = req.body.latitude;
   // const created_on = Date().now();
   const user_id = 1;
-     db.query(`
+
+
+  const id = generateRandomString()
+
+  cardDatabase[id] = {
+    title,
+    description,
+    longitude,
+    latitude,
+    id
+  }
+  
+  console.log(cardDatabase);
+
+  res.redirect("/maps")
+
+
+  db.query(
+    `
   INSERT INTO maps (title, description, longitude, latitude, created_on, user_id)
   VALUES ($1, $2, $3, $4, $5, $6)
-  RETURNING *`, [title, description, longitude, latitude, '2021-03-11 09:30:00',  user_id])
-      .then((result) => result.rows[0])
-      .catch((err) => console.log(err.message));
+  RETURNING *`,
+    [title, description, longitude, latitude, "2021-03-11 09:30:00", user_id]
+  )
+    .then((result) => result.rows[0])
+    .catch((err) => console.log(err.message));
+
+
 });
-
-
-
-
-
-
-
-
-
 
 app.get("/edit_map", (req, res) => {
   const user = req.session.id;
-  res.render("edit_map", {user: user});
+  res.render("edit_map", { user: user });
 });
 
-
-
-
-
 app.post("/edit_map", (req, res) => {
-
   const title = req.body.title;
   const description = req.body.description;
   const longitude = req.body.longitude;
@@ -210,10 +256,12 @@ app.post("/edit_map", (req, res) => {
   // const created_on = Date().now();
   const user_id = 1;
 
-  db.query(`UPDATE maps (title, description, longitude, latitude, created_on, user_id)
+  db.query(
+    `UPDATE maps (title, description, longitude, latitude, created_on, user_id)
   SET ($1, $2, $3, $4, $5, $6)
   RETURNING *;`,
-  [title, description, longitude, latitude, '2021-03-11 09:30:00',  user_id])
+    [title, description, longitude, latitude, "2021-03-11 09:30:00", user_id]
+  )
     .then((result) => result.rows[0])
     .catch((err) => console.log(err.message));
   res.redirect("/maps");
@@ -224,12 +272,14 @@ app.post("/edit_map", (req, res) => {
 
 
 
-app.post("/delete", (req, res) => {
-
+app.post("/delete/:id", (req, res) => {
   // Need to add a code to delete
+
+  delete cardDatabase[req.params.id];
 
   res.redirect("/maps");
 });
+
 
 
 
